577f4e45834cfd0c5cfa3162a8aaf49b24e9509d
avatarnguyen <nguyenanh@hotmail.de>
2020-11-19 20:54:48 +0100

    debug heroku v15

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 const app = express();
-
 dotenv.config();
 // const config = require("./server/config/keys");
 // const mongoose = require("mongoose");
