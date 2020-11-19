8025f3137eb59bceaa8e3c8e598da84fbf92d90c
avatarnguyen <nguyenanh@hotmail.de>
2020-11-19 18:27:53 +0100

    setting up env

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import dialogflowRoutes from './server/routes/dialogflow.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 
 
 // const config = require("./server/config/keys");
@@ -39,8 +40,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-// const CONNECTION_URL = 'mongodb+srv://anhfirstmernproject:2vMJuQl4178Cblbw@cluster0.1palz.mongodb.net/<dbname>?retryWrites=true&w=majority'
-const CONNECTION_URL = 'mongodb+srv://anhfirstmernproject:2vMJuQl4178Cblbw@cluster0.1palz.mongodb.net/diagnostdb?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 8000;
 
 // mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
